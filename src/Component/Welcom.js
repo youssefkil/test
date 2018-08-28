097d1ec767c96abb5be7dbc905a27029b69a214e
youssefkil <yous.mavie@gmail.com>
2018-08-28 22:21:05 +0200

    ggs

--- src/Component/Welcom.js
+++ src/Component/Welcom.js
@@ -4,7 +4,7 @@ import React , {Component} from 'react';
 class Welcom extends Component {
     render() {
         return(   
-            <h1>hello every {this.props.name} </h1>  
+            <h1>hello every to home {this.props.name} </h1>  
           
         );
     }
