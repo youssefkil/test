b86cec56c759757a731a6d55cb0d1f9868c24747
youssefkil <yous.mavie@gmail.com>
2018-08-28 22:20:27 +0200

    gg

--- src/Component/Welcom.js
+++ src/Component/Welcom.js
@@ -4,7 +4,7 @@ import React , {Component} from 'react';
 class Welcom extends Component {
     render() {
         return(   
-            <h1>hello every body {this.props.name} </h1>  
+            <h1>hello every {this.props.name} </h1>  
           
         );
     }
---
+++ src/Component/toto.js
@@ -0,0 +1,12 @@
+import React , {Component} from 'react';
+
+
+class toto extends Component {
+    render() {
+        return(   
+            <h1>hello every body {this.props.name} </h1>  
+          
+        );
+    }
+}
+export default toto;
